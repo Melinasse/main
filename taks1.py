76d9db6014982bce9fe4c722b7e9e7fefe487758
Azam4t17 <AyzekMendes@gmail.com>
2024-07-10 00:25:12 +0500

    Add files via upload

---
+++ taks1.py
@@ -0,0 +1,5 @@
+#1st program
+a=9
+b=5
+c=0.5
+print((a**c)*b)
---
+++ task4.py
@@ -0,0 +1,4 @@
+#4st program
+a=13.42; b=42.13 #присвоение значения переменной
+print(((int(a))==int(b*100)%100) or int(a*100)%100==int(b))
+print(((int(a))==int(b*100)%100) and int(a*100)%100==int(b))
